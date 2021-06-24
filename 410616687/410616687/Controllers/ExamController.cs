e671fc869eac38464da8ee3d2fb2b788fef53579
王韋勛 <open00096@gmail.com>
2021-06-24 19:37:53 +0800

    水果

---
+++ 410616687/410616687/Controllers/ExamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _410616687.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
